9a1962ef4829a8bb58e36f988c3d1f51c9dc1587
Alexander <alexander.0894-rp@hotmail.com>
2021-12-02 20:41:56 +0000

    bety

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -2,10 +2,9 @@
 /**
  * binary_tree_size - measures the size of a binary tree
  * @tree:  pointer to the root node of the tree
- * 
+ *
  * Return: Return: size of a binary tree
  */
-
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t t_size = 0;
